2c13c5681707ec4194ce0cd0c0baf249b41e8388
Unknown <denisevbaalen@gmail.com>
2017-01-12 17:29:02 +0100

    Initial files

---
+++ src/nl/ru/ai/exercise6/Convert.java
@@ -0,0 +1,59 @@
+package nl.ru.ai.exercise6;
+
+public class Convert
+{
+
+  public static void main(String[] args)
+  {
+    /*
+     * Test for tail recursion
+     */
+    xsy(3);
+    System.out.println();
+    /*
+     * Test for left recursion
+     */
+    reverse("ykraps");
+    System.out.println();
+    /*
+     * Test for middle recursion
+     */
+    downUp(5);
+    System.out.println();
+  }
+
+  private static void xsy(int i)
+  {
+    if(i==0)
+      System.out.print("y");
+    else
+    {
+      System.out.print("x");
+      xsy(i-1);
+    }
+  }
+  
+  private static void reverse(String s)
+  {
+    if(s.length()==0)
+      return;
+    else
+    {
+      reverse(s.substring(1));
+      System.out.print(s.charAt(0));
+    }
+  }
+  
+  private static void downUp(int i)
+  {
+    if(i==0)
+      System.out.print("!");
+    else
+    {
+      System.out.print(i);
+      downUp(i-1);
+      System.out.print(i);
+    }
+  }
+
+}
